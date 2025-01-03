98a7dd052d8fbcc7c6f8264870d21a586d75a7c7
Guhxs1010 <133364786+Guhxs1010@users.noreply.github.com>
2025-01-03 19:03:29 -0300

    meio

---
+++ js/letra.js
@@ -0,0 +1,9 @@
+/* ----- TYPING EFFECT ----- */
+let typingEffect = new Typed(".typedText", {
+    strings: ["Designer", "Desenvolvedor", "Gustavo Lenni"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 2000,
+  });
+  
